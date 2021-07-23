a943b13b456fba224f9c9fd836556f46451e74c3
andy <deifmo@yandex.ru>
2021-07-23 17:07:59 +0300

    Implemented system of adding new reviews

--- br/views.py
+++ br/views.py
@@ -1,11 +1,26 @@
 import datetime
 
-from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
 from django.db.models import Avg, Count
-
-from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from .models import Author, Genre, Book, Review
+from .forms import SimpleForm
+
+
+class SimpleFormView(generic.edit.FormView):
+    template_name = 'br/simple_form.html'
+    form_class = SimpleForm
+    success_url = '/simple_form/'
+
+    def get(self, request, *args, **kwargs):
+        print(self.request.GET)
+        return super().get(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        print(self.request.POST)
+        print(form.cleaned_data)
+
+        return super().form_valid(form)
 
 
 class IndexListView(generic.list.ListView):
@@ -63,5 +78,42 @@ class BookDetailView(generic.detail.DetailView):
         context = super().get_context_data(**kwargs)
         book = context['book']
         authors = book.authors.all()
+
+        book_reviews = Review.objects.filter(book=book).order_by('-pub_date')
+
+        if self.request.user.is_authenticated and book_reviews.filter(owner=self.request.user):
+            my_review = book_reviews.get(owner=self.request.user)
+            other_reviews = book_reviews.exclude(owner=self.request.user)
+        else:
+            my_review = None
+            other_reviews = book_reviews
+
+        context['my_review'] = my_review
+        context['other_reviews'] = other_reviews
         context['authors'] = authors
         return context
+
+
+class ReviewCreateView(generic.edit.CreateView):
+    model = Review
+    fields = ['rating', 'title', 'text']
+    template_name = 'br/add_review.html'
+    query_pk_and_slug = True
+
+    def get_success_url(self):
+        book = get_object_or_404(Book, id=self.kwargs['pk'], slug=self.kwargs['slug'])
+        return book.get_absolute_url()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(self.kwargs)
+        book = get_object_or_404(Book, id=self.kwargs['pk'], slug=self.kwargs['slug'])
+        context['book'] = book
+        return context
+
+    def form_valid(self, form):
+        new_review = form.save(commit=False)
+        new_review.owner = self.request.user
+        book = get_object_or_404(Book, id=self.kwargs['pk'], slug=self.kwargs['slug'])
+        new_review.book = book
+        return super().form_valid(form)
