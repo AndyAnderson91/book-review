720e431f77ebeba8df4d1f8559d329186dd9d9cd
andy <deifmo@yandex.ru>
2021-08-03 15:18:49 +0300

    Asterisk disabled on fields, statring to work on images

--- br/urls.py
+++ br/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, re_path
 from django.contrib.auth.decorators import login_required
 from . import views
@@ -24,3 +26,6 @@ urlpatterns = [
     # Search results page.
     path('search/', views.SearchListView.as_view(), name='search'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
