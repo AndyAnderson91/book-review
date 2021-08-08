90ff2ff898914a101570792e6f10885913bd3abc
andy <deifmo@yandex.ru>
2021-08-08 21:28:20 +0300

    Mega redirection bug fixed

---
+++ br/templatetags/custom_tags.py
@@ -0,0 +1,34 @@
+from django.urls import reverse
+from urllib.parse import urlencode
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def url_replace(context, **kwargs):
+    """
+    Replaces values of url parameters (such as page=value in pagination).
+    """
+    query = context.get('request').GET.copy()
+    query.update(kwargs)
+    return urlencode(query)
+
+
+@register.simple_tag(takes_context=True)
+def url_next(context, **kwargs):
+    """
+    If 'Login' or 'Sign up' buttons are pressed, current path is captured in 'next' parameter,
+    so user will be redirected back to his last page after login or registration.
+    Exception is case where previous page is login page or registration page.
+    In this case user will be redirected to index page, so it prevents looping.
+    Same about 'Logout'. User stays at page he was before pressing 'Logout' button.
+    Exception is 'My reviews' page. Only authenticated users have access to it,
+    so after logout user is redirected to index page.
+    """
+    cur_path = context.get('request').path
+
+    if cur_path not in [reverse('users:login'), reverse('users:register'), reverse('br:my_reviews')]:
+        return '?' + urlencode(kwargs)
+    else:
+        return ''
